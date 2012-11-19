24de891ef9d0941ca59800698a9af620a550a8cc
Milton Bittencourt <milton@batman.(none)>
2012-11-19 18:14:28 -0200

    Todos os métodos de minimização prontos

--- LucasJM_MiltonBN_AF_GR/src/controller/Minimizador.java
+++ LucasJM_MiltonBN_AF_GR/src/controller/Minimizador.java
@@ -9,25 +9,24 @@ import model.Transicao;
 
 public class Minimizador {
 
-	// Se um estado inalcancavel leva para ele mesmo e trata como inalcancavel =/
-	public List<Estado> getInalcancaveis(Automato e) {
-		List<Estado> inalcancaveis = new ArrayList<Estado>(e.getEstados());
-		List<Estado> alcancaveis = new ArrayList<Estado>();
+//	// Se um estado inalcancavel leva para ele mesmo e trata como inalcancavel =/
+//	public List<Estado> getInalcancaveis(Automato e) {
+//		List<Estado> inalcancaveis = new ArrayList<Estado>(e.getEstados());
+//		List<Estado> alcancaveis = new ArrayList<Estado>();
+//
+//		for (Estado estado : inalcancaveis) {
+//			List<Transicao> transicoes = estado.getTransicoes();
+//			for (Transicao transicao : transicoes) {
+//				if (!alcancaveis.contains(transicao.getEstadoDestino())) {
+//					alcancaveis.add(estado);
+//				}
+//			}
+//		}
+//		inalcancaveis.removeAll(alcancaveis);
+//		return inalcancaveis;
+//	}
 
-		for (Estado estado : inalcancaveis) {
-			List<Transicao> transicoes = estado.getTransicoes();
-			for (Transicao transicao : transicoes) {
-				if (!alcancaveis.contains(transicao.getEstadoDestino())) {
-					alcancaveis.add(estado);
-				}
-			}
-		}
-		inalcancaveis.removeAll(alcancaveis);
-		return inalcancaveis;
-	}
-
-	// esse tá bala
-	public List<Estado> getInalcancaveis2(Automato e) {
+	private List<Estado> getInalcancaveis(Automato e) {
 
 		List<Estado> alcancaveis = new ArrayList<Estado>();
 		List<Estado> temp = new ArrayList<Estado>();
@@ -65,12 +64,13 @@ public class Minimizador {
 		return inalcancaveis;
 	}
 
-	public Automato removeInalcancaveis(Automato e, List<Estado> inalcancaveis) {
+	public Automato removeInalcancaveis(Automato e) {
+		List<Estado> inalcancaveis = getInalcancaveis(e);
 		e.getEstados().removeAll(inalcancaveis);
 		return e;
 	}
 
-	public List<Estado> getEstadosMortos(Automato automato){
+	private List<Estado> getEstadosMortos(Automato automato){
 		List<Estado> mortos = new ArrayList<Estado>();
 		List<Estado> allEstado = automato.getEstados();
 		
@@ -81,24 +81,45 @@ public class Minimizador {
 				}
 			} else{
 				if(!estado.isEstFinal()  && (estado.getTransicoes().size() == 0 || isTransicaoNaoRecursiva(estado))){
-					
+					mortos.add(estado);
 				}
 			}
 		}
 		
 		return mortos;
 	}
+	
+	public Automato removeEstadosMortos(Automato e){
+
+		List<Estado> mortos = getEstadosMortos(e);
+		List<Estado> allEstados = e.getEstados();
+		
+		for (Estado estado : allEstados) {
+			List<Transicao> removedTransicoes = new ArrayList<Transicao>();
+			List<Transicao> transicoes = estado.getTransicoes();
+			for (Transicao transicao : transicoes) {
+				if(mortos.contains(transicao)){
+					removedTransicoes.add(transicao);
+				}
+			}
+			if(removedTransicoes.size() > 0){
+				estado.getTransicoes().removeAll(removedTransicoes);
+			}
+			
+		}
+		
+		e.getEstados().removeAll(mortos);
+		
+		return e;
+	}
 
 	private boolean isTransicaoNaoRecursiva(Estado estado) {
-
 		List<Transicao> transicoes = estado.getTransicoes();
-		
 		for (Transicao transicao : transicoes) {
 			if(!transicao.getEstadoDestino().equals(estado)){
 				return true;
 			}
 		}
-		
 		return false;
 	}
 	
