c5f607e2e122590024f1d4774a5a88854edebf0b
barbaresco <barbaresco.rodrigo@gmail.com>
2012-11-19 17:06:45 -0200

    Minimização em andamento

--- LucasJM_MiltonBN_AF_GR/src/controller/Minimizador.java
+++ LucasJM_MiltonBN_AF_GR/src/controller/Minimizador.java
@@ -1,7 +1,86 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Automato;
+import model.Estado;
+import model.Transicao;
+
 public class Minimizador {
 
+	// Se um estado inalcancavel leva para ele mesmo e trata como inalcancavel =/
+	public List<Estado> getInalcancaveis(Automato e) {
+		List<Estado> inalcancaveis = new ArrayList<Estado>(e.getEstados());
+		List<Estado> alcancaveis = new ArrayList<Estado>();
 
+		for (Estado estado : inalcancaveis) {
+			List<Transicao> transicoes = estado.getTransicoes();
+			for (Transicao transicao : transicoes) {
+				if (!alcancaveis.contains(transicao.getEstadoDestino())) {
+					alcancaveis.add(estado);
+				}
+			}
+		}
+		inalcancaveis.removeAll(alcancaveis);
+		return inalcancaveis;
+	}
 
+	// esse tá bala
+	public List<Estado> getInalcancaveis2(Automato e) {
+
+		List<Estado> alcancaveis = new ArrayList<Estado>();
+		List<Estado> temp = new ArrayList<Estado>();
+		List<Estado> inalcancaveis = new ArrayList<Estado>(e.getEstados());
+
+		alcancaveis.add(e.getEstadoInicial());
+
+		boolean fim = true;
+		boolean breaker = false;
+
+		while (fim) {
+			breaker = false;
+			temp.clear();
+			temp.addAll(alcancaveis);
+			for (Estado alcancavel : temp) {
+				List<Transicao> transicoes = alcancavel.getTransicoes();
+				for (Transicao transicao : transicoes) {
+					if (!alcancaveis.contains(transicao.getEstadoDestino())) {
+						alcancaveis.add(transicao.getEstadoDestino());
+						breaker = true;
+						break;
+					}
+				}
+				if (breaker) {
+					fim = true;
+					break;
+				} else {
+					fim = false;
+				}
+			}
+		}
+
+		inalcancaveis.removeAll(alcancaveis);
+		
+		return inalcancaveis;
+	}
+
+	public Automato removeInalcancaveis(Automato e, List<Estado> inalcancaveis) {
+		e.getEstados().removeAll(inalcancaveis);
+		return e;
+	}
+
+	public List<Estado> getEstadosMortos(Automato automato){
+		List<Estado> mortos = new ArrayList<Estado>();
+		List<Estado> allEstado = automato.getEstados();
+		
+		for (Estado estado : allEstado) {
+			if(!estado.isEstFinal() && estado.getTransicoes() == null || estado.getTransicoes().size() == 0){
+				
+			}
+		}
+		
+		return mortos;
+	}
+	
 }
